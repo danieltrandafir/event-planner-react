a812900d6511df5455e81177ff660f345d08c9a9
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-03-23 20:26:17 +0000

    useEffect to LS & LS util funct

--- src/components/FormPlanner.js
+++ src/components/FormPlanner.js
@@ -15,7 +15,7 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Typography } from "@mui/material";
 
-export const FormPlanner = () => {
+export const FormPlanner = ({ addPlan }) => {
   const [value, setValue] = React.useState(null);
 
   const initialValues = {
@@ -51,7 +51,7 @@ export const FormPlanner = () => {
       location,
       notes,
     };
-    console.log(myPlan);
+    addPlan(myPlan);
   };
 
   const formik = useFormik({
--- src/pages/CreatePlan.js
+++ src/pages/CreatePlan.js
@@ -1,15 +1,35 @@
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
+import { useEffect, useState } from "react";
 
 import { FormPlanner } from "../components/FormPlanner";
 import { PageTitle } from "../components/PageTitle";
+import { getFromLocalStorage } from "../utils/getFromLocalStorage";
 
 export const CreatePlan = () => {
+  const [plans, setPlans] = useState();
+
+  useEffect(() => {
+    const plansFromLS = getFromLocalStorage("plans", []);
+
+    setPlans(plansFromLS);
+  }, []);
+
+  const addPlan = (plan) => {
+    const plansFromLS = getFromLocalStorage("plans", []);
+
+    plansFromLS.push(plan);
+
+    localStorage.setItem("plans", JSON.stringify(plansFromLS));
+
+    setPlans(plansFromLS);
+  };
+
   return (
     <Container maxWidth="lg">
       <Stack>
         <PageTitle title={"Make Your Plan!"} />
-        <FormPlanner />
+        <FormPlanner addPlan={addPlan} />
       </Stack>
     </Container>
   );
