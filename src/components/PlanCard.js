7a8420db067f616fe54292bd3fee1b647e033f52
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-03-28 00:47:28 +0100

    style PlanCard

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -4,6 +4,9 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { Container, Stack } from "@mui/system";
+
+import { PageTitle } from "./PageTitle";
 
 export const PlanCard = () => {
   let planInfoFromLS = localStorage.getItem("plans");
@@ -12,23 +15,36 @@ export const PlanCard = () => {
   console.log(planInfoFromLS);
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {planInfoFromLS[0].name} {planInfoFromLS[0].surname}
-        </Typography>
-        <Typography variant="h5">{planInfoFromLS[0].email}</Typography>
-        <Typography variant="h5">{planInfoFromLS[0].location}</Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Container>
+      <PageTitle title={"See your plans!"} />
+
+      <Card sx={{ maxWidth: 345, boxShadow: "-3px 4px 6px 1px #000" }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="../images/debby-hudson-vb0r4K_J1h8-unsplash.jpg"
+          title="tbc"
+        />
+        <CardContent>
+          <Stack textAlign={"center"}>
+            <Typography variant="h3">{planInfoFromLS[0].title}</Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              Organised by: {planInfoFromLS[0].name} {planInfoFromLS[0].surname}
+            </Typography>
+            <Typography variant="h5">{planInfoFromLS[0].email}</Typography>
+            <Typography variant="h5">
+              Location: {planInfoFromLS[0].location}
+            </Typography>
+          </Stack>
+        </CardContent>
+        <CardActions sx={{ justifyContent: "center" }}>
+          <Button size="small" variant="contained" color="error">
+            Remove
+          </Button>
+          <Button size="small" variant="contained" color="primary">
+            View Plan
+          </Button>
+        </CardActions>
+      </Card>
+    </Container>
   );
 };
