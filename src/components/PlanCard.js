2fc3e489497c9089f272604033f2f14ddf206b96
Daniel Trandafir <danieltrandafir@live.co.uk>
2023-03-29 00:58:58 +0100

    flex myplans & remove plan from LS

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -4,40 +4,57 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Container, Stack } from "@mui/system";
+import Stack from "@mui/material/Stack";
 
+import { getFromLocalStorage } from "../utils/getFromLocalStorage";
 
-export const PlanCard = ({ plan, setPlans }) => {
+export const PlanCard = ({ plans, setPlans }) => {
+  // remove plans from MyPlans page
+  const handleRemovePlan = () => {
+    // retrieve list of plans from LS
+    const planListFromLS = getFromLocalStorage("plans", []);
+    // create new array after filter for condition
+    const newList = planListFromLS.filter(
+      (eachPlan) => eachPlan.id !== plans.id
+    );
+    localStorage.setItem("plans", JSON.stringify(newList));
+    setPlans(newList);
+  };
+
   return (
-    <Container>
-
-      <Card sx={{ maxWidth: '18rem', boxShadow: "-3px 4px 6px 1px #000" }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image="../images/debby-hudson-vb0r4K_J1h8-unsplash.jpg"
-          title="tbc"
-        />
-        <CardContent>
-          <Stack textAlign={"center"}>
-            <Typography variant="h3">{plan.title}</Typography>
-            <Typography gutterBottom variant="h5" component="div">
-              Organised by: {plan.name}{' '}{plan.surname}
-            </Typography>
-            <Typography variant="h5">{plan.email}</Typography>
-            <Typography variant="h5">
-              Location: {plan.location}
-            </Typography>
-          </Stack>
-        </CardContent>
-        <CardActions sx={{ justifyContent: "center" }}>
-          <Button size="small" variant="contained" color="error">
-            Remove
-          </Button>
-          <Button size="small" variant="contained" color="primary">
-            View Plan
-          </Button>
-        </CardActions>
-      </Card>
-    </Container>
+    <Card
+      sx={{ mt: "1rem", minWidth: "16rem", boxShadow: "-3px 4px 6px 1px #000" }}
+    >
+      <CardMedia
+        sx={{ height: 140 }}
+        image="../images/debby-hudson-vb0r4K_J1h8-unsplash.jpg"
+        title="tbc"
+      />
+      <CardContent>
+        <Stack textAlign={"center"}>
+          <Typography variant="h4">{plans.title}</Typography>
+          <Typography gutterBottom variant="subtitle1" component="div">
+            Organised by: {plans.name} {plans.surname}
+          </Typography>
+          <Typography variant="subtitle1">{plans.email}</Typography>
+          <Typography variant="subtitle1">
+            Location: {plans.location}
+          </Typography>
+        </Stack>
+      </CardContent>
+      <CardActions sx={{ justifyContent: "center" }}>
+        <Button
+          size="small"
+          variant="contained"
+          color="error"
+          onClick={handleRemovePlan}
+        >
+          Remove
+        </Button>
+        <Button size="small" variant="contained" color="primary">
+          View Plan
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
--- src/pages/MyPlans.js
+++ src/pages/MyPlans.js
@@ -1,22 +1,49 @@
-import { Stack } from "@mui/system";
 import { PlanCard } from "../components/PlanCard";
 import { PageTitle } from "../components/PageTitle";
 import Container from "@mui/material/Container";
-import { Box } from "@mui/material";
+import { Alert, AlertTitle, Box } from "@mui/material";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItem from "@mui/material/ListItem";
+import Button from "@mui/material/Button";
 
+import { useNavigate } from "react-router-dom";
 
 export const MyPlans = ({ plans, setPlans }) => {
+  const navigate = useNavigate();
   return (
-    <Container maxWidth="lg"
-      sx={{ justifyContent: "space-evenly" }}>
+    <Container sx={{ justifyContent: "space-evenly" }}>
       <PageTitle title={"See your plans!"} />
-      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly" }}>
-
-        {plans
-          ? plans.map((plan) => (
-            <PlanCard plan={plan} setPlan={setPlans} />
+      <Box
+        gap={2}
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-evenly",
+        }}
+      >
+        {plans.length > 0 ? (
+          plans.map((plans) => (
+            <PlanCard key={plans.id} plans={plans} setPlans={setPlans} />
           ))
-          : "No Plans made"}
+        ) : (
+          <Alert severity="warning">
+            <AlertTitle>Oops, no plans!</AlertTitle>
+            What are you waiting for? Make some plans!
+          </Alert>
+        )}
+
+        <ListItem disablePadding sx={{ width: "max-content" }}>
+          <Button
+            onClick={() => {
+              navigate("/create-plan");
+            }}
+            sx={{ border: "1px solid black", margin: "auto" }}
+            variant="contained"
+          >
+            Create Plan
+          </Button>
+        </ListItem>
       </Box>
     </Container>
   );
